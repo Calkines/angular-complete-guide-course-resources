30f2d99dd6a1d5e57e5aa8236ca75c26f5fa6491
Rafael CP <rafael_it@hotmail.com>
2024-09-21 22:56:25 -0300

    feat: remove FormsModule from app module

--- att/03/01/src/app/app.module.ts
+++ att/03/01/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserComponent } from './user/user.component';
