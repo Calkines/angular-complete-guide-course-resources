b99b5c00e5e58bebe3465632585069a4a37e7fbd
Rafael CP <rafael_it@hotmail.com>
2024-09-21 23:14:00 -0300

    feat: just one module like older commit

--- att/03/01/src/app/app.module.ts
+++ att/03/01/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { CardComponent } from './shared/card/card.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
-  // Add standalone components here
-  imports: [BrowserModule, TasksComponent, HeaderComponent, UserComponent],
-  // Add not standalone compoents here
-  declarations: [AppComponent],
+  imports: [FormsModule, BrowserModule],
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    UserComponent,
+    CardComponent,
+    NewTaskComponent,
+    TaskComponent,
+    HeaderComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
